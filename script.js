7935c47dfbdf57b4e8b1acbb009cd87465190dec
Mhmd Zmeter <mhmdz@Mhmds-MacBook-Pro.local>
2024-12-11 10:48:23 +0200

    Added JavaScript file

---
+++ script.js
@@ -0,0 +1,10 @@
+const img = document.getElementById("bigimg");
+function phones(phone) {
+  img.src = phone;
+  
+}
+const content =document.getElementById("content");
+
+function colors(color){
+  content.style.background = color;
+}
